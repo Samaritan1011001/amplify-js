a13cdc4eed74478dd4c07bfb287dd582a8e91042
ManojNB <manojnb95@gmail.com>
2025-01-03 15:46:19 -0800

    chore: update gradle files

---
+++ packages/rtn-web-browser/android/src/test/kotlin/com/amazonaws/amplify/rtnwebbrowser/WebBrowserModule.kt
@@ -0,0 +1,13 @@
+package com.amazonaws.amplify.rtnwebbrowser
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class SimpleUnitTest {
+
+	@Test
+	fun testAddition() {
+		val sum = 2 + 2
+		assertEquals(4, sum)
+	}
+}
